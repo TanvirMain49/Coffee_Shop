9cbaf8b92d37ef3890f8e4f8741c4ee129de9eeb
TanvirMain49 <tanvirmahin607@gmail.com>
2025-08-05 23:00:13 +0600

    change in the script file

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -20,6 +20,7 @@ const CategorySection = () => {
 
   return (
     <div className="relative bg-secondary py-24 flex items-center justify-center gap-10">
+      {/* text content */}
       <div className="max-w-2xl w-full">
         <RenderText
           title="Coffee"
@@ -39,14 +40,17 @@ const CategorySection = () => {
         />
       </div>
 
+      {/* card swipe images */}
       <div>
         <CardSwipe images={images} autoplayDelay={2000} slideShadows={false} />
       </div>
 
+      {/* Donut outline image */}
       <div className="absolute top-[2%] left-[3%]">
         <img src="/Donut.png" alt="Donut" className="w-40 h-40" />
       </div>
-
+      
+      {/* Coffee outline image */}
       <div className="absolute bottom-[8%] right-[46%]">
         <img
           src="/Coffee-outline.png"
