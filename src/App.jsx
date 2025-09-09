97d8aeff27c84f02b7af672bcc54ff835198a698
TanvirMain49 <tanvirmahin607@gmail.com>
2025-09-09 20:48:53 +0600

    Resolve the config

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
-<<<<<<< Updated upstream
 import Cart from "./Pages/Cart";
 import Dashboard from "./Pages/Dashboard";
-=======
 import Login from "./Pages/login";
 import Signup from "./Pages/Signup";
 import Profile from "./Pages/Profile";
 import Layout from "./components/Custom/Layout";
->>>>>>> Stashed changes
 
 function App() {
 
   return (
     <div>
       <Routes>
-<<<<<<< Updated upstream
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/admin" element={<Dashboard></Dashboard>} />
-=======
         <Route path="/" element={
           <Layout>
             <Home />
@@ -32,7 +27,6 @@ function App() {
             <Profile/>
           </Layout>
           } />
->>>>>>> Stashed changes
       </Routes>
     </div>
   );
